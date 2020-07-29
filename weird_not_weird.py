4ebb4aebb927aea5d71ceccf6952c2886832d7e7
Aadarsh Raj <68873194+aadarshraj4321@users.noreply.github.com>
2020-07-29 21:54:50 +0530

    Add files via upload

---
+++ weird_not_weird.py
@@ -0,0 +1,14 @@
+def weird_not_weird(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif (n % 2 == 0) and (n>=2 and n<=5):
+        print("Not Weird")
+    elif (n % 2 == 0) and (n>=6 and n<=20):
+        print("Weird")
+    elif (n % 2 == 0) and (n>20):
+        print("Not Weird")
+
+
+
+n= int(input())
+weird_not_weird(n)
