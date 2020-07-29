b2e3719588965a78134386e0b4309b743e9d2bff
Aadarsh Raj <68873194+aadarshraj4321@users.noreply.github.com>
2020-07-29 21:55:28 +0530

    Add files via upload

---
+++ arthmetic_operators.py
@@ -0,0 +1,26 @@
+"""
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+
+"""
+
+
+def arithmetic_perators(a,b):
+    #print(f"sum of two number is : {a + b}")
+    #print(f"difference of two nunmber is : {a-b}")
+    #print(f"product of two number is : {a*b}")
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+
+
+arithmetic_perators(a,b)
+
